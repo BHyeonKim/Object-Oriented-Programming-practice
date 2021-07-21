7c58125af5f362e4fab19a9913d92afc4241c7c7
BHyeonKim <rlaqhguse@gmail.com>
2021-07-22 02:07:14 +0900

    change struct to class

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,21 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
-typedef struct {
+class Account {
+private:
 	int accountNumber;	// 계좌번호
 	char* name;
-	int deposit;
-}Account;
-
-Account accounts[100];
-int accountNum = 0;  // 계좌수
+	int money;
+public:
+	Account(int, const char*, int);
+	int getAccountNumber();
+	char* getName();
+	int getMoney();
+	void deposit(int money);
+	void withdraw(int money);
+};
 
 int showMenu();
-
 void openNewAccount();
 void deposit();
 void withdraw();
 void showAccountInfo();
+
+Account* accounts[100];
+int accountNum = 0;  // 계좌수
+
 
 enum{OPEN_NEW_ACCOUNT=1, DEPOSIT, WITHDRAW, SHOW_ACCOUNT_INFO, EXIT };
 int main() {
@@ -57,23 +66,57 @@ int showMenu() {
 	cout << "4. 계좌정보 전체 출력\n";
 	cout << "5. 프로그램 종료\n";
 	cout << "선택: "; cin >> select;
+	cout << '\n';
 
 	return select;
 }
 
+Account::Account(int accountNumber, const char* name, int money) {
+	this->accountNumber = accountNumber;
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+	this->money = money;
+}
+
+int Account::getAccountNumber()
+{
+	return accountNum;
+}
+
+char* Account::getName()
+{
+	return name;
+}
+
+int Account::getMoney()
+{
+	return money;
+}
+
+void Account::deposit(int money)
+{
+	this->money += money;
+}
+
+void Account::withdraw(int money)
+{
+	this->money -= money;
+}
+
 void openNewAccount() {
+	int accountNumber; //계좌번호
 	cout << "[계좌개설]\n";
-	cout << "계좌ID: ";		cin >> accounts[accountNum].accountNumber;
+	cout << "계좌ID: ";		cin >> accountNumber;
 
-	cout << "이름: ";
-	char temp[20];
-	cin >> temp;
-	accounts[accountNum].name = new char[strlen(temp) + 1];
 
-	cout << "입금액: ";		cin >> accounts[accountNum].deposit;
+	char name[20];
+	cout << "이름: ";		cin >> name;
+
+	int money;
+	cout << "입금액: ";		cin >> money;
 	cout << '\n';
 
-	accountNum++;
+	accounts[accountNum++] = new Account(accountNumber, name, money);
 }
 
 void deposit() {
@@ -83,8 +126,8 @@ void deposit() {
 	cout << "입금액: ";		cin >> money;
 
 	for (int i = 0; i < accountNum; i++)
-		if (accounts[i].accountNumber == account)
-			accounts[i].deposit += money;
+		if (accounts[i]->getAccountNumber() == account)
+			accounts[i]->deposit(money);
 
 	cout << "입금완료\n\n";
 }
@@ -96,21 +139,21 @@ void withdraw() {
 	cout << "출금액: ";		cin >> money;
 
 	for (int i = 0; i < accountNum; i++)
-		if (accounts[i].accountNumber == account)
-			if (accounts[i].deposit >= money){
-				accounts[i].deposit -= money;
+		if (accounts[i]->getAccountNumber() == account)
+			if (accounts[i]->getMoney() >= money){
+				accounts[i]->withdraw(money);
 			}else {
-				cout << "잔액부족\n\n";
+				cout << "잔액부족\n" << '\n';
 				return;
 			}
 
-	cout << "출금완료\n\n";
+	cout << "출금완료\n" << '\n';
 }
 
 void showAccountInfo() {
 	for (int i = 0; i < accountNum; i++) {
-		cout << "계좌ID: " << accounts[i].accountNumber << '\n';
-		cout << "이름 " << accounts[i].name << '\n';
-		cout << "잔액: " << accounts[i].deposit << '\n';
+		cout << "계좌ID: " << accounts[i]->getAccountNumber() << '\n';
+		cout << "이름 " << accounts[i]->getName() << '\n';
+		cout << "잔액: " << accounts[i]->getMoney() << '\n' << '\n';
 	}
 }
